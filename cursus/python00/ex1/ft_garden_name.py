be919167eeb0dc66ed36a27b461c8f420d92c4f3
Oscar Duran Marti <oduran-m@car5s4.42barcelona.com>
2026-06-17 17:44:27 +0200

    python00+

---
+++ cursus/python00/ex1/ft_garden_name.py
@@ -0,0 +1,4 @@
+def ft_garden_name():
+    nombreGarden = input("Enter garden name: ")
+    print(f"Garden: {nombreGarden}")
+    print("Status: Growing well!")
---
+++ cursus/python00/ex2/ft_plot_area.py
@@ -0,0 +1,5 @@
+def ft_plot_area():
+    lenght = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    totalarea = lenght * width
+    print(f"Plot area: {totalarea}")
